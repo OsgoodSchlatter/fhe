3f71a5a4a19372346760e12224ef12e5ab58e30d
Osgood <eloiposeidon@gmail.com>
2022-08-17 17:37:08 +0200

    feat: full subtract one bit for division

---
+++ utils/full_subtract_one_bit.c
@@ -0,0 +1,32 @@
+#include <tfhe/tfhe.h>
+#include <tfhe/tfhe_io.h>
+#include <stdio.h>
+#include <time.h>
+
+/***
+ * completes the truth table of three inputs (a,b,c_in) for subtraction on one bit
+ * helpful in multiplier
+ * @arg subtract: subtract of two bits
+ * @arg a: bit 1
+ * @arg b: bit 2
+ * @arg carry_in: carry_in bit that will be turned into carry_out. Then, it will be carry_in for next full_subtract
+ *
+ * @return subtract and carry_out
+ * ***/
+void full_subtract_one_bit(LweSample *subtract, LweSample *a, LweSample *b, LweSample *carry_in, const TFheGateBootstrappingCloudKeySet *bk)
+{
+    const LweParams *in_out_params = bk->params->in_out_params;
+    // carries & result & temp
+    LweSample *temp = new_LweSample_array(4, in_out_params);
+    LweSample *not_a = new_LweSample_array(1, in_out_params);
+    bootsNOT(not_a, a, bk);
+    // layer 1
+    bootsXOR(temp, a, b, bk);
+    // computing temp_3 = not(temp_0)
+    bootsNOT(temp + 3, temp, bk);
+    bootsAND(temp + 1, not_a, b, bk);
+    // layer 2
+    bootsXOR(subtract, carry_in, temp, bk);
+    bootsAND(temp + 2, carry_in, temp + 3, bk);
+    bootsOR(carry_in, temp + 1, temp + 2, bk);
+}
