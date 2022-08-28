da2ab011383b7c6f4a8c9cc2bc3968445a2dd4fe
Osgood <eloiposeidon@gmail.com>
2022-08-28 15:01:13 +0200

    fix in divison

--- utils/compare_bit_own.c
+++ utils/compare_bit_own.c
@@ -2,6 +2,7 @@
 #include <tfhe/tfhe_io.h>
 #include <stdio.h>
 
+// returns 4, 2 or 1 respectively wether a=b, a>b or a<b
 void compare_bit_own(LweSample *result, LweSample *a, LweSample *b, const TFheGateBootstrappingCloudKeySet *bk)
 {
     LweSample *inf_or_eq_or_sup = new_LweSample_array(3, bk->params->in_out_params);
--- utils/diviser.c
+++ utils/diviser.c
@@ -13,10 +13,10 @@
  *
  * @return encrypted result
  * ***/
-LweSample *diviser(LweSample *result, LweSample *dividende, LweSample *divisor, const TFheGateBootstrappingCloudKeySet *bk)
+
+int *diviser(LweSample *result, LweSample *dividende, LweSample *divisor, const TFheGateBootstrappingCloudKeySet *bk)
 {
     LweSample *temp_rest = new_LweSample_array(16, bk->params->in_out_params);
-    LweSample *rest = new_LweSample_array(16, bk->params->in_out_params);
     LweSample *temp_dividende = new_LweSample_array(16, bk->params->in_out_params);
     LweSample *temp_dividende_copy = new_LweSample_array(16, bk->params->in_out_params);
     LweSample *quotient = new_LweSample_array(16, bk->params->in_out_params);
@@ -24,15 +24,16 @@ LweSample *diviser(LweSample *result, LweSample *dividende, LweSample *divisor, 
     bootsCONSTANT(zero, 0, bk);
     LweSample *one = new_LweSample_array(1, bk->params->in_out_params);
     bootsCONSTANT(one, 1, bk);
+    int iter = 16; // should be 16 when normal
 
     //  looping over 16 bits of dividende
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < iter; i++)
     {
         // initialisation
         if (i == 0)
         {
-            bootsCOPY(temp_dividende, dividende + 15, bk);
-            for (int i = 1; i < 16; i++)
+            bootsCOPY(temp_dividende, dividende + iter - 1, bk);
+            for (int i = 1; i < iter; i++)
             {
                 bootsCOPY(temp_dividende + i, zero, bk);
             }
@@ -43,7 +44,7 @@ LweSample *diviser(LweSample *result, LweSample *dividende, LweSample *divisor, 
 
         // the quotient is being added either 0 if dividende < divisor or 1 if opposite.
         // we check the 15th bit of temp_rest: it is 0 if dividende < divisor or 1 if opposite
-        bootsMUX(quotient, temp_rest + 15, zero, one, bk);
+        bootsMUX(quotient, temp_rest + iter - 1, zero, one, bk);
 
         // we offset by one the bit to change in quotient to modify its value.
         offset(quotient, quotient, 1, bk);
@@ -60,23 +61,27 @@ LweSample *diviser(LweSample *result, LweSample *dividende, LweSample *divisor, 
         }
         // we offset by one bit temp_dividende_copy to cater dividende next bit
         offset(temp_dividende_copy, temp_dividende_copy, 1, bk);
-        bootsCOPY(temp_dividende_copy, dividende + 15 - i, bk);
+        bootsCOPY(temp_dividende_copy, dividende + iter - 1 - i, bk);
 
         // -------------CASE B---------------
         // here, temp_dividende > divisor then temp_dividende's value should be = to temp_rest
         // we offset by one bit temp_rest to cater dividende next bit, just like before
         offset(temp_rest, temp_rest, 1, bk);
-        bootsCOPY(temp_rest, dividende + 15 - i, bk);
+        bootsCOPY(temp_rest, dividende + iter - 1 - i, bk);
 
         // then we must choose whether to copy temp_dividende_copy or temp_rest to temp_dividende.
         for (int j = 0; j < i; j++)
         {
-            bootsMUX(temp_dividende + j, temp_rest + 15, temp_dividende_copy + j, temp_rest + j, bk);
+            bootsMUX(temp_dividende + j, temp_rest + iter - 1, temp_dividende_copy + j, temp_rest + j, bk);
         }
     }
 
+    // we subtract one last time and change the quotient accordingly.
+    full_subtract(temp_rest, temp_dividende, divisor, bk);
+    bootsMUX(quotient, temp_rest + iter - 1, zero, one, bk);
+
     // finally, we copy the quotient into the result, to be decrypted
-    for (int i = 0; i < 16; i++)
+    for (int i = 0; i < iter; i++)
     {
         bootsCOPY(result + i, quotient + i, bk);
     }
