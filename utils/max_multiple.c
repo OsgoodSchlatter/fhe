ec3834584633b91471722f5f3101b5325a63b728
OsgoodSchlatter <eloiposeidon@gmail.com>
2023-05-23 13:17:03 +0000

    WIP sort max, need to adapt alice, oracle and verif

--- utils/max_multiple.c
+++ utils/max_multiple.c
@@ -36,3 +36,56 @@ void max_multiple(LweSample *result, LweSample *offers[], int offerNbr,
         bootsCOPY(&result[j], &tmp[j], keyset);
     }
 }
+
+// performs  max
+void max_mult_inputs_sort(LweSample *result[], LweSample *offers[], int offerNbr,
+                          const TFheGateBootstrappingCloudKeySet *keyset)
+{
+    const LweParams *in_out_params = keyset->params->in_out_params;
+
+    LweSample *zero = new_LweSample_array(1, keyset->params->in_out_params);
+    bootsCONSTANT(zero, 0, keyset);
+    LweSample *one = new_LweSample_array(1, keyset->params->in_out_params);
+    bootsCONSTANT(one, 1, keyset);
+
+    LweSample *tmp_max = new_LweSample_array(16, in_out_params);
+
+    LweSample *tmp_sub = new_LweSample_array(16, in_out_params);
+
+    // we have the array in which we know the positions of the data to copy
+    LweSample *number_to_copy[offerNbr];
+
+    LweSample *offers_cpy[offerNbr];
+
+    for (int offer = 0; offer < offerNbr; offer++)
+    {
+        max_mult_inputs(tmp_max, offers, offerNbr, keyset);
+
+        for (int offer_tmp = 0; offer_tmp < offerNbr; offer_tmp++)
+        {
+            offers_cpy[offer_tmp] = new_LweSample_array(16, in_out_params);
+            for (int i = 0; i < 16; i++)
+                bootsCOPY(offers_cpy[offer_tmp] + i, offers[offer_tmp] + i, keyset);
+
+            full_subtract(tmp_sub, offers_cpy[offer_tmp], tmp_max, keyset);
+            number_to_copy[offer_tmp] = new_LweSample_array(1, in_out_params);
+            // if tmp sub + 15 = 1  it means that we're not on the good number so we add one to number_to_copy
+            // if tmp sub + 15 = 0 we are on the good number, so we add zero
+            bootsMUX(number_to_copy[offer_tmp], tmp_sub + 15, one, zero, keyset);
+        }
+        for (int offer_tmp = 0; offer_tmp < offerNbr; offer_tmp++)
+        {
+            for (int i = 0; i < 16; i++)
+            {
+                // here, if number to copy = 1, it means we are not on the random that is the max, so we just recopy that number in the list for the next turn.
+                // if number to copy = 0, it means we are on the max, and therefor should not copy it back. So we add zeroes
+                bootsMUX(offers[offer_tmp] + i, number_to_copy[offer_tmp], offers_cpy[offer_tmp] + i, zero, keyset);
+            }
+        }
+
+        for (int i = 0; i < 16; i++)
+        {
+            bootsCOPY(&result[offer][i], &tmp_max[i], keyset);
+        }
+    }
+}
